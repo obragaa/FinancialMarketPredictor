1c90ee68f7e53f68c5066447fe31f5cce8232528
Felipe Braga Duarte <60896979+obragaa@users.noreply.github.com>
2024-05-07 13:34:58 -0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
 # src/config.py
 
-API_KEY = 'V0DFC6Q9KSVKSCOD'  # Substitua por sua chave de API real
+API_KEY = 'token'  # Substitua por sua chave de API real
 BASE_URL = 'https://www.alphavantage.co/query'
